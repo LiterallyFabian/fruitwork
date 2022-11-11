1d8e7153614653fc9879085c1b0482f3204a068b
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-11-11 19:32:40 +0100

    Create sprite class

---
+++ include/Sprite.h
@@ -0,0 +1,29 @@
+#ifndef FRUITWORK_SPRITE_H
+#define FRUITWORK_SPRITE_H
+
+#include <string>
+#include <SDL.h>
+#include "Component.h"
+
+namespace fruitwork
+{
+
+    class Sprite : public Component {
+
+    public:
+        static Sprite *getInstance(int x, int y, int w, int h, const std::string& text);
+
+        void draw() const override;
+
+        ~Sprite() override;
+
+    protected:
+        Sprite(int x, int y, int w, int h, const std::string& text);
+
+    private:
+        SDL_Texture *spriteTexture;
+    };
+
+} // fruitwork
+
+#endif //FRUITWORK_SPRITE_H
---
+++ src/Sprite.cpp
@@ -0,0 +1,36 @@
+#include "Sprite.h"
+#include "System.h"
+#include "Constants.h"
+#include <SDL_image.h>
+#include <iostream>
+
+namespace fruitwork
+{
+    Sprite::Sprite(int x, int y, int w, int h, const std::string &textureName) : Component(x, y, w, h)
+    {
+        SDL_Surface *surface = IMG_Load((constants::gResPath + "images/" + textureName).c_str());
+
+        if (surface == nullptr)
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load image: %s", textureName.c_str());
+
+        spriteTexture = SDL_CreateTextureFromSurface(sys.get_renderer(), surface);
+        SDL_FreeSurface(surface);
+    }
+
+    Sprite *Sprite::getInstance(int x, int y, int w, int h, const std::string &textureName)
+    {
+        return new Sprite(x, y, w, h, textureName);
+    }
+
+    void Sprite::draw() const
+    {
+        SDL_RenderCopy(sys.get_renderer(), spriteTexture, nullptr, &get_rect());
+    }
+
+    Sprite::~Sprite()
+    {
+        SDL_DestroyTexture(spriteTexture);
+    }
+
+
+} // fruitwork
