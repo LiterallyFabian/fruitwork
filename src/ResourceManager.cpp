af28fa0607fe695809ffae7940d0b6a64ac486db
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-11-12 09:23:49 +0100

    Check files in resource manager

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,21 +1,50 @@
-#include "ResourceManager.h"
 #include "Constants.h"
+#include "ResourceManager.h"
+#include <sys/stat.h>
+#include <string>
+#include <iostream>
+#include "SDL.h"
 
 namespace fruitwork
 {
+    /**
+     * Checks if a file exists at the given path.
+     * @param path The path to check.
+     * @return True if the file exists, false otherwise.
+     * @see https://stackoverflow.com/a/12774387
+     */
+    inline bool file_exists(const std::string &path)
+    {
+        struct stat buffer{};
+        return (stat(path.c_str(), &buffer) == 0);
+    }
+
     std::string ResourceManager::getTexturePath(const std::string &textureName)
     {
-        return constants::gResPath + "images/" + textureName;
+        std::string path = constants::gResPath + "images/" + textureName;
+        if (!file_exists(path))
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load image: %s", textureName.c_str());
+
+        return path;
     }
 
     std::string ResourceManager::getFontPath(const std::string &fontName)
     {
-        return constants::gResPath + "fonts/" + fontName;
+        std::string path = constants::gResPath + "fonts/" + fontName;
+        if (!file_exists(path))
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load font: %s", fontName.c_str());
+
+        return path;
     }
 
     std::string ResourceManager::getAudioPath(const std::string &clipName)
     {
-        return constants::gResPath + "audio/" + clipName;
+        std::string path = constants::gResPath + "sounds/" + clipName;
+        if (!file_exists(path))
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load audio clip: %s", clipName.c_str());
+
+        return path;
     }
+
 
 } // fruitwork
