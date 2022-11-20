875ddb83e54d9c9460d9f0c230e00628d049bc90
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-11-20 10:07:25 +0100

    Add alignment to responsive sprite

--- include/ResponsiveSprite.h
+++ include/ResponsiveSprite.h
@@ -11,13 +11,27 @@ namespace fruitwork
      */
     class ResponsiveSprite : public Sprite {
     public:
+        enum class Alignment {
+            TOP_LEFT,
+            TOP_CENTER,
+            TOP_RIGHT,
+            CENTER_LEFT,
+            CENTER,
+            CENTER_RIGHT,
+            BOTTOM_LEFT,
+            BOTTOM_CENTER,
+            BOTTOM_RIGHT
+        };
 
-        static ResponsiveSprite *getInstance(int x, int y, int w, int h, const std::string &textureName);
+        static ResponsiveSprite *getInstance(int x, int y, int w, int h, const std::string &textureName, Alignment alignment = Alignment::CENTER);
 
         void draw() const override;
 
     protected:
-        ResponsiveSprite(int x, int y, int w, int h, const std::string &textureName);
+        ResponsiveSprite(int x, int y, int w, int h, const std::string &textureName, Alignment alignment = Alignment::CENTER);
+
+    private:
+        Alignment alignment;
     };
 
 } // fruitwork
--- src/ResponsiveSprite.cpp
+++ src/ResponsiveSprite.cpp
@@ -3,25 +3,55 @@
 
 namespace fruitwork
 {
-    ResponsiveSprite *ResponsiveSprite::getInstance(int x, int y, int w, int h, const std::string &textureName)
+    ResponsiveSprite *ResponsiveSprite::getInstance(int x, int y, int w, int h, const std::string &textureName, Alignment alignment)
     {
-        return new ResponsiveSprite(x, y, w, h, textureName);
+        return new ResponsiveSprite(x, y, w, h, textureName, alignment);
     }
 
-    ResponsiveSprite::ResponsiveSprite(int x, int y, int w, int h, const std::string &textureName)
-            : Sprite(x, y, w, h, textureName) {}
+    ResponsiveSprite::ResponsiveSprite(int x, int y, int w, int h, const std::string &textureName, Alignment alignment)
+            : Sprite(x, y, w, h, textureName), alignment(alignment) {}
 
     void ResponsiveSprite::draw() const
     {
         SDL_Rect r = get_rect();
 
-        if (r.w > r.h) // a landscape image
-        {
+        if (r.w > r.h) // a landscape image, width is greater than height
             r.w = r.h;
-        }
         else
-        {
             r.h = r.w;
+
+        switch (alignment)
+        {
+            case Alignment::TOP_LEFT:
+                break;
+            case Alignment::TOP_CENTER: // move right by half the difference
+                r.x += (get_rect().w - r.w) / 2;
+                break;
+            case Alignment::TOP_RIGHT: // move right by the difference
+                r.x += get_rect().w - r.w;
+                break;
+            case Alignment::CENTER_LEFT: // move down by half the difference
+                r.y += (get_rect().h - r.h) / 2;
+                break;
+            case Alignment::CENTER: // move right by half the difference, then down by half the difference
+                r.x += (get_rect().w - r.w) / 2;
+                r.y += (get_rect().h - r.h) / 2;
+                break;
+            case Alignment::CENTER_RIGHT:
+                r.x += get_rect().w - r.w;
+                r.y += (get_rect().h - r.h) / 2;
+                break;
+            case Alignment::BOTTOM_LEFT:
+                r.y += get_rect().h - r.h;
+                break;
+            case Alignment::BOTTOM_CENTER:
+                r.x += (get_rect().w - r.w) / 2;
+                r.y += get_rect().h - r.h;
+                break;
+            case Alignment::BOTTOM_RIGHT:
+                r.x += get_rect().w - r.w;
+                r.y += get_rect().h - r.h;
+                break;
         }
 
         SDL_RenderCopy(sys.get_renderer(), spriteTexture, nullptr, &r);
