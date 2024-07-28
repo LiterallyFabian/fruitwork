bb3a4d4dd79717b8b0d19551cd8e94ae193ffe58
Sofia Lindgren <hi@sofia.kiwi>
2024-07-28 11:22:36 +0200

    Rename rect variables

--- src/Component.cpp
+++ src/Component.cpp
@@ -49,110 +49,110 @@ namespace fruitwork
 
     const SDL_Rect &Component::getAbsoluteRect() const
     {
-        SDL_Rect thisRect = getRect();
-        SDL_Rect parentRect; // if no component parent, we use the window size
+        SDL_Rect localRect = getRect();
+        SDL_Rect parentAbsoluteRect; // if no component parent, we use the window size
         if (parent == nullptr)
         {
             SDL_Window *window = fruitwork::sys.getWindow();
-            SDL_GetWindowSize(window, &parentRect.w, &parentRect.h);
-            parentRect.x = 0;
-            parentRect.y = 0;
+            SDL_GetWindowSize(window, &parentAbsoluteRect.w, &parentAbsoluteRect.h);
+            parentAbsoluteRect.x = 0;
+            parentAbsoluteRect.y = 0;
         }
         else
         {
-            parentRect = parent->getAbsoluteRect();
+            parentAbsoluteRect = parent->getAbsoluteRect();
         }
 
-        SDL_Rect rect = thisRect;
+        SDL_Rect newAbsoluteRect = localRect;
 
         switch (anchor)
         {
             case Anchor::LEGACY_TOP_LEFT:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y + thisRect.y;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y + localRect.y;
                 break;
             case Anchor::TOP_LEFT:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y - thisRect.y;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y;
                 break;
             case Anchor::TOP_CENTER:
-                rect.x = parentRect.x + thisRect.x + parentRect.w / 2 - thisRect.w / 2;
-                rect.y = parentRect.y - thisRect.y;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w / 2 - localRect.w / 2;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y;
                 break;
             case Anchor::TOP_RIGHT:
-                rect.x = parentRect.x + thisRect.x + parentRect.w - thisRect.w;
-                rect.y = parentRect.y - thisRect.y;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w - localRect.w;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y;
                 break;
             case Anchor::CENTER_LEFT:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y - thisRect.y + parentRect.h / 2 - thisRect.h / 2;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y + parentAbsoluteRect.h / 2 - localRect.h / 2;
                 break;
             case Anchor::CENTER:
-                rect.x = parentRect.x + thisRect.x + parentRect.w / 2 - thisRect.w / 2;
-                rect.y = parentRect.y - thisRect.y + parentRect.h / 2 - thisRect.h / 2;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w / 2 - localRect.w / 2;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y + parentAbsoluteRect.h / 2 - localRect.h / 2;
                 break;
             case Anchor::CENTER_RIGHT:
-                rect.x = parentRect.x + thisRect.x + parentRect.w - thisRect.w;
-                rect.y = parentRect.y - thisRect.y + parentRect.h / 2 - thisRect.h / 2;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w - localRect.w;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y + parentAbsoluteRect.h / 2 - localRect.h / 2;
                 break;
             case Anchor::BOTTOM_LEFT:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y - thisRect.y + parentRect.h - thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y + parentAbsoluteRect.h - localRect.h;
                 break;
             case Anchor::BOTTOM_CENTER:
-                rect.x = parentRect.x + thisRect.x + parentRect.w / 2 - thisRect.w / 2;
-                rect.y = parentRect.y - thisRect.y + parentRect.h - thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w / 2 - localRect.w / 2;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y + parentAbsoluteRect.h - localRect.h;
                 break;
             case Anchor::BOTTOM_RIGHT:
-                rect.x = parentRect.x + thisRect.x + parentRect.w - thisRect.w;
-                rect.y = parentRect.y - thisRect.y + parentRect.h - thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w - localRect.w;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y + parentAbsoluteRect.h - localRect.h;
                 break;
 
             case Anchor::TOP_STRETCH:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y - thisRect.y; // based on Unity implementation of stretch, might look weird
-                rect.w = parentRect.w - thisRect.w - thisRect.x;
-                rect.h = thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y - localRect.y; // based on Unity implementation of stretch, might look weird
+                newAbsoluteRect.w = parentAbsoluteRect.w - localRect.w - localRect.x;
+                newAbsoluteRect.h = localRect.h;
                 break;
             case Anchor::CENTER_STRETCH:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y + parentRect.h / 2 - thisRect.h / 2 - thisRect.y;
-                rect.w = parentRect.w - thisRect.x - thisRect.w;
-                rect.h = thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y + parentAbsoluteRect.h / 2 - localRect.h / 2 - localRect.y;
+                newAbsoluteRect.w = parentAbsoluteRect.w - localRect.x - localRect.w;
+                newAbsoluteRect.h = localRect.h;
                 break;
             case Anchor::BOTTOM_STRETCH:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y + parentRect.h - thisRect.h - thisRect.y;
-                rect.w = parentRect.w - thisRect.x - thisRect.w;
-                rect.h = thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y + parentAbsoluteRect.h - localRect.h - localRect.y;
+                newAbsoluteRect.w = parentAbsoluteRect.w - localRect.x - localRect.w;
+                newAbsoluteRect.h = localRect.h;
                 break;
             case Anchor::STRETCH:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y + thisRect.y;
-                rect.w = parentRect.w - thisRect.w * 2;
-                rect.h = parentRect.h - thisRect.h * 2;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y + localRect.y;
+                newAbsoluteRect.w = parentAbsoluteRect.w - localRect.w * 2;
+                newAbsoluteRect.h = parentAbsoluteRect.h - localRect.h * 2;
                 break;
             case Anchor::STRETCH_LEFT:
-                rect.x = parentRect.x + thisRect.x;
-                rect.y = parentRect.y + thisRect.y;
-                rect.w = thisRect.w;
-                rect.h = parentRect.h - thisRect.y - thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y + localRect.y;
+                newAbsoluteRect.w = localRect.w;
+                newAbsoluteRect.h = parentAbsoluteRect.h - localRect.y - localRect.h;
                 break;
             case Anchor::STRETCH_RIGHT:
-                rect.x = parentRect.x + parentRect.w - thisRect.w + thisRect.x;
-                rect.y = parentRect.y + thisRect.y;
-                rect.w = thisRect.w;
-                rect.h = parentRect.h - thisRect.y - thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + parentAbsoluteRect.w - localRect.w + localRect.x;
+                newAbsoluteRect.y = parentAbsoluteRect.y + localRect.y;
+                newAbsoluteRect.w = localRect.w;
+                newAbsoluteRect.h = parentAbsoluteRect.h - localRect.y - localRect.h;
                 break;
             case Anchor::STRETCH_CENTER:
-                rect.x = parentRect.x + thisRect.x + parentRect.w / 2 - thisRect.w / 2;
-                rect.y = parentRect.y + thisRect.y;
-                rect.w = thisRect.w;
-                rect.h = parentRect.h - thisRect.y - thisRect.h;
+                newAbsoluteRect.x = parentAbsoluteRect.x + localRect.x + parentAbsoluteRect.w / 2 - localRect.w / 2;
+                newAbsoluteRect.y = parentAbsoluteRect.y + localRect.y;
+                newAbsoluteRect.w = localRect.w;
+                newAbsoluteRect.h = parentAbsoluteRect.h - localRect.y - localRect.h;
                 break;
         }
 
-        absoluteRect = rect;
+        absoluteRect = newAbsoluteRect;
         return absoluteRect;
     }
 
